172a1ca923c5444fb63a65acafba5481897c627a
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-27 16:50:18 +0600

    i have added here new components for this world of the countries

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -3,19 +3,20 @@ import './country.css'
 
 const Country = ({ country, handleVisitCountries }) => {
     const [visited, setVisited] = useState(false)
-    const { name, independent, flags, population } = country
-    const { common } = name
-    const { png } = flags
+    const { name, independent, flags, population, continents } = country
+    const { common, official } = name
+    const { png, alt } = flags
     const handleVisit = () => {
         setVisited(!visited)
         handleVisitCountries(country)
     }
     return (
         <div className={`country ${visited && 'country-visited'}`}>
-            <img width={'370px'} height={'220px'} src={png} alt="" />
+            <img width={'370px'} height={'220px'} src={png} alt={alt} />
             <h3>Name: {common} </h3>
-            <p>Independent: {independent ? 'Independent' : 'Not Free'}</p>
-            <p>Population: {population}</p>
+            <p>Continents: {continents} || {independent ? 'Independent' : 'Not Independent'}</p>
+            <p> Population: {population}</p>
+            <p>Official Name: {official}</p>
             <button className={visited ? 'btn-visited' : 'btn-notVisited'} onClick={handleVisit}>{visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     );
